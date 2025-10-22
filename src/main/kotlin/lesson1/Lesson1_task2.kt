fbdba4dca1d8bfea4b7b6dc08da0d4bb54151b22
Wan <morozko889@gmail.com>
2025-10-22 18:25:23 +0300

    KS-1-2

---
+++ src/main/kotlin/lesson1/Lesson1_task2.kt
@@ -0,0 +1,15 @@
+package org.example.lesson1
+
+
+fun main() {
+    val countOfOrders: Int = 75
+    val gratitudeForOrders: String = "Спасибо за заказ! Вы заказали ${countOfOrders} позиций"
+
+    println(gratitudeForOrders)
+
+    var countOfEmployees: Int = 2000
+    //println(countOfEmployees)
+
+    countOfEmployees = 1999
+    println(countOfEmployees)
+}
